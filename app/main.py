cc6bb36f0042f9a40224c28d58d2ea3d4f898ae0
Abhiram Kamsali <abhiramk2305@gmail.com>
2024-08-16 23:25:05 +0530

    auth commits

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from . import models
 import time
 from sqlalchemy.orm import Session
 from .database import engine,get_db
-from .routers import user,post
+from .routers import user,post,auth
 
 models.Base.metadata.create_all(bind = engine)
 
@@ -25,6 +25,7 @@ while True:
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,13 +1,15 @@
-from .. import schemas,utils,models
-from fastapi import FastAPI,Response,status,HTTPException, Depends,APIRouter
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-from ..database import engine,get_db
+from .. import schemas, utils, models
+from ..database import get_db
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/users',
+    tags=['Users']
+)
 
-@router.post('/users', status_code=status.HTTP_201_CREATED,response_model = schemas.UserOut)
+@router.post('', status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
     new_user = models.User(**user.model_dump())
@@ -16,11 +18,11 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@router.get('/users/{id}',response_model = schemas.UserOut)
-def get_user(id:int,db:Session = Depends(get_db)):
+@router.get('/{id}', response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'user with {id} is not found')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with ID {id} not found')
     
     return user
